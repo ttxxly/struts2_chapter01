2121c25a2949ad04882140feba6e126f19f79d3a
ttxxly <ttxxly@gmail.com>
2018-08-26 22:29:03 +0800

    调整文件结构、

--- src/com/darklovy/hello/HelloAction.java
+++ src/com/darklovy/hello/HelloAction.java
@@ -1,5 +1,18 @@
 package com.darklovy.hello;
 
-public class HelloAction {
+import com.opensymphony.xwork2.Action;
+
+/*
+ * 
+ */
+public class helloaction implements Action{
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("执行了 HelloAction");
+		return "success";
+	}
+	
 
 }
